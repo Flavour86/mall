89474483c8c44e5c3e65f80e804b28c0dd1b4cdd
江典恩 <982624@git.sdp.nd>
2018-03-22 20:58:21 +0800

    首页

--- src/pages/index/i18n/zh.js
+++ src/pages/index/i18n/zh.js
@@ -6,5 +6,10 @@ export default {
   hotCategory: '热门品类',
   hotCards: '热门品牌',
   matchTitle: '人气搭配',
-  recommendTitle: '推荐搭配'
+  recommendTitle: '推荐搭配',
+  clothTitle: '潮流趋势',
+  trTitle: '经典裤装',
+  shoeTitle: '时尚鞋履',
+  bagTitle: '实用包类',
+  decorationTitle: '潮人配饰'
 }
